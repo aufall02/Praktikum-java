9830e75e14870c95c09a76732ca05e0082fdf107
aufall02 <aufaluzumaki@gmail.com>
2022-07-13 09:51:29 +0700

    Tugas praktikum 5 Polimorphysm

---
+++ src/praktikum5/ChildClass.java
@@ -0,0 +1,8 @@
+package praktikum5;
+
+public class ChildClass extends ParentClass{
+    ChildClass(String say){
+        super(say);
+        System.out.println("Hallo "+say+"saya anak class");
+    }
+}
---
+++ src/praktikum5/ParentClass.java
@@ -0,0 +1,7 @@
+package praktikum5;
+
+class ParentClass {
+    ParentClass(String say){
+        System.out.println("Hallo "+say+", saya Bapak class ");
+    }
+}
